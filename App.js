6bea2895f5d89872c28c756a0648fd8831df9ffc
henrychao <henrychao24@163.com>
2020-12-17 15:08:03 +0800

    add image-picker

--- App.js
+++ App.js
@@ -2,10 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import ImagePickerExample from './sample/ImagePickerExample';
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <ImagePickerExample />
       <StatusBar style="auto" />
     </View>
   );
